9f290c109e0bc158d9dd99bbd50f77539f8dac13
Alexandra <sashaf708@gmail.com>
2025-07-29 00:38:54 +0400

    feat(api): configure API instance for HTTP requests

---
+++ frontend/src/shared/api/instance.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  },
+);
+
+export const api = axios;
