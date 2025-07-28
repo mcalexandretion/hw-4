0033c2933ff8d1bbb1ae06776048513c0b764a02
Alexandra <sashaf708@gmail.com>
2025-07-29 00:36:01 +0400

    feat(user): define TypeScript types for user entity

---
+++ frontend/src/entities/user/model/types.ts
@@ -0,0 +1,11 @@
+export interface User {
+  id: string;
+  name: string;
+  surName?: string;
+  fullName?: string;
+  email: string;
+  birthDate?: string;
+  telephone?: string;
+  employment?: string;
+  userAgreement?: boolean;
+}
